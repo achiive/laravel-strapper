c34c8cdebc8886749106882eafba3ba7086bd799
WolfieZero <neil@wolfiezero.com>
2016-03-08 10:29:28 +0000

    mis-named the directory

--- config/app.php
+++ config/app.php
@@ -210,7 +210,7 @@ return [
          * Laravel Strapper...
          */
         'JWTAuth'  => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'Helper'   => App\Achiive\Supports\Helper::class,
+        'Helper'   => App\Achiive\Modules\Helper::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
